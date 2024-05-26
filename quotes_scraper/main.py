604dcfc68137710f9767cc2ebfcbcd4a4fdfbffb
DawidDziekan <dziekan.dawid@wp.pl>
2024-05-26 12:59:24 +0200

    peach

--- quotes_scraper/main.py
+++ quotes_scraper/main.py
@@ -2,6 +2,8 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from quotes_scraper.spiders.quotes_spider import QuotesSpider
 from quotes_scraper.spiders.authors_spider import AuthorsSpider
+from import_to_db import importtodb
+
 
 def main():
     process = CrawlerProcess(settings=get_project_settings())
@@ -9,5 +11,7 @@ def main():
     process.crawl(QuotesSpider)
     process.start()
 
+    importtodb()
+    
 if __name__ == "__main__":
     main()
